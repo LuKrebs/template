417107cdd9b2baa8b7c6313fab9541310ba14f62
Luciano Krebs <luciano.krebs@hotmail.com>
2017-09-09 19:35:43 -0300

    initial commit

---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,17 @@
+$('.datepicker').pickadate({
+  today: 'Hoje',
+  clear: 'Limpar',
+  close: 'Ok',
+  closeOnSelect: true // Close upon selecting a date,
+});
+$(".button-collapse").sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true, // Choose whether you can drag to open on touch screens,
+});
+
+$('.fixed-action-btn').openFAB();
+$('.fixed-action-btn').closeFAB();
+$('.fixed-action-btn.toolbar').openToolbar();
+$('.fixed-action-btn.toolbar').closeToolbar();
