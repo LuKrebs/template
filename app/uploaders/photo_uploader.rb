e3e4d9546196c7fadd9399a928a11016c4cb1448
Luciano Krebs <luciano.krebs@hotmail.com>
2017-09-24 22:02:28 -0300

    hotfix image uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -18,8 +18,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   process eager: true  # Force version generation at upload time.
 
-  process convert: 'jpg'
-
   version :standard do
     resize_to_fit 800, 600
   end
