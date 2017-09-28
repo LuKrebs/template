eff1fe3e5108d76a987fc19dada83cec6d6aab8e
Luciano Krebs <luciano.krebs@hotmail.com>
2017-09-28 00:22:04 -0300

    date_picker fixed when user scroll down the page working properly

--- app/assets/javascripts/getmdl.js
+++ app/assets/javascripts/getmdl.js
@@ -35,6 +35,43 @@ $(document).ready(function() {
     }).error(function(response){
     });
   });
-
 });
 
+function navbarFixedFunction() {
+  var searchBarHeight = parseInt($("#fixedBookingRow").css("height").replace("px", ""));
+  var searchBarPosition = $("#fixedBookingRow").position()['top'];
+  var bodyScrollTop = $("body").scrollTop();
+  console.log("body scrollTop: " + bodyScrollTop);
+  console.log("navbar position: " + searchBarHeight);
+
+  if (bodyScrollTop > searchBarHeight + searchBarPosition) {
+    if ($(window).width() < 425) {
+      $(".mobileAndWidthSmallHide").hide();
+    }
+    $("#fixedBookingRow").css("position", "fixed");
+    $("#fixedBookingRow").css("top", "0");
+    $("#fixedBookingRow").css("right", "0");
+    $("#fixedBookingRow").css("left", "0");
+    $("#fixedBookingRow").css("margin", "auto");
+    $("#fixedBookingRow").css("z-index", "1000");
+    $("#fixedBookingRow").css("backgroundColor", "rgba(0,0,0,0.9)");
+    $(".bookRowInside").css("marginBottom", '0px');
+    $(".bookRow").css("marginBottom", '0px');
+    $(".input-field .material-icons.prefix").css('color', 'white');
+  }
+  else {
+    var display = $(".mobileAndWidthSmallHide").css('display');
+    if ($(window).width() < 425) {
+      $(".mobileAndWidthSmallHide").show();
+    }
+    else if ($(window).width() > 425 && display == "none") {
+      $(".mobileAndWidthSmallHide").show();
+    }
+    $("#fixedBookingRow").css("backgroundColor", "transparent");
+    $("#fixedBookingRow").css("position", "relative");
+    $("#fixedBookingRow").css("margin-bottom", "20vh");
+    $(".bookRowInside").css("marginBottom", '20px');
+    $(".bookRow").css("marginBottom", '20px');
+    $(".input-field .material-icons.prefix").css('color', "rgba(0, 0, 0, 0.87)");
+  }
+}
