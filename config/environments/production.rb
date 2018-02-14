674a19750d3b7f0ea07d38ff1e9a28ae1dd62504
Luciano Krebs <luciano.krebs@hotmail.com>
2018-02-14 13:57:17 -0200

    fix email SMTPAuthenticationError

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "template-hq.herokuapp.com" }
-
+  config.action_mailer.raise_delivery_errors = true
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
